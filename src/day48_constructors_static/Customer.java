2b679954a9bff3d7cd8617da749953d9c7e3635c
Fikret <fburgazli@hotmail.com>
2021-05-29 10:55:45 -0400

    added customer class and getter/setter methods with constructor

---
+++ src/day48_constructors_static/Customer.java
@@ -0,0 +1,35 @@
+package day48_constructors_static;
+
+public class Customer {
+
+    private String name;
+    private int id;
+
+    public Customer(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
